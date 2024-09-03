3bf5034a0e5b6f39950361c5eb9e49d2acde94d2
Luiza Marinho <luiza.calheirosm@gmail.com>
2024-09-03 20:04:46 -0300

    Update media-necessaria.c
    Questão 4: Faça um programa que receba três notas de um aluno, calcule e mostre a média aritmética e a mensagem constante na tabela a seguir. Aos alunos que ficaram para exame, calcule e mostre a nota que deverão tirar para serem aprovados, considerando que a média exigida é 6,0.

--- media-necessaria.c
+++ media-necessaria.c
@@ -3,27 +3,31 @@
 float func_media(float n1, float n2, float n3)
 {
     float media;
+    float exame;
     media = (n1 + n2 + n3) / 3;
 
     if (media < 3.0)
     {
-        printf("Reprovado");
+        printf("Reprovado \n");
     }
     else if (media >= 3.0 && media < 7.0)
     {
-        printf("Exame");
+        exame = 12 - media;
+        printf("Exame precisando de %0.1f \n", exame);
     }
     else
     {
-        printf("Aprovado");
+        printf("Aprovado \n");
     }
+
+    return 0;
 }
 
 int main()
 {
     float nota1, nota2, nota3;
 
-    printf("Insira 3 notas");
+    printf("Insira 3 notas \n");
     scanf("%f %f %f", &nota1, &nota2, &nota3);
 
     func_media(nota1, nota2, nota3);
