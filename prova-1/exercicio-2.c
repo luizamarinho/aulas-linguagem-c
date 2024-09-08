021faa38e7151b25ef292ce7d31ff6ee86beadd6
Luiza Marinho <luiza.calheirosm@gmail.com>
2024-09-08 14:52:24 -0300

    Create exercicio-2.c

---
+++ prova-1/exercicio-2.c
@@ -0,0 +1,37 @@
+// Crie uma função que verifique se um ano fornecido é bissexto. Retorne 1 se for
+// bissexto, caso contrário, a função não deve retornar nada. Um ano é bissexto se for
+// divisível por 4, exceto os divisíveis por 100, a menos que também sejam divisíveis
+// por 400.
+
+#include <stdio.h>
+
+ehbissexto(int teste)
+{
+
+    if (teste % 4 != 0)
+    {
+        // nao eh divisivel por 4, entao nao tem como ser bissexto
+        return 0;
+    }
+
+    if (teste % 100 == 0 && teste % 400 != 0)
+    {
+        return 0;
+    }
+
+    return 1;
+}
+
+int main()
+{
+    int ano;
+    int resultado;
+
+    printf("Insira um ano ");
+    scanf("%d", &ano);
+
+    if (ehbissexto(ano) == 1)
+    {
+        printf("%d\n", ehbissexto(ano));
+    }
+}
