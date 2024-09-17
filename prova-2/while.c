1c82e7d28767ce062e9789a328f1451ae81c30d4
Luiza Marinho <luiza.calheirosm@gmail.com>
2024-09-17 19:27:45 -0300

    Create while.c

---
+++ prova-2/while.c
@@ -0,0 +1,17 @@
+// criar um programa que repita a palavra "batata" cinco vezes
+
+#include <stdio.h>
+
+int main()
+{
+    int batata;
+    int indice;
+
+    indice = 0;
+
+    while (indice <= 5)
+    {
+        printf("Batata");
+        indice = indice + 1;
+    }
+}
