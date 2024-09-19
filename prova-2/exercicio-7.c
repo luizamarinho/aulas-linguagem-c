f1e12a93f78dd309bad73e5c55d885e54eb3f1b5
Luiza Marinho <luiza.calheirosm@gmail.com>
2024-09-19 14:24:33 -0300

    Create exercicio-7.c

---
+++ prova-2/exercicio-7.c
@@ -0,0 +1,32 @@
+// Crie um programa que peça ao usuário para inserir as notas de 5 alunos. Para
+// cada aluno, o programa deve calcular a média. Se a média for maior ou igual a 7.0,
+// o aluno é aprovado; caso contrário, ele é reprovado. Ao final, o programa deve exibir
+// a média de cada aluno e se ele foi aprovado ou reprovado. (0.3 ponto)
+// Entrada: 5 notas (uma por aluno).
+// Saída: média de cada aluno e status de aprovação.
+
+#include <stdio.h>
+
+int main()
+{
+    int nota;
+    int indice = 1;
+
+    while (indice <= 5)
+    {
+        printf("Insira a nota ");
+        scanf("%d", &nota);
+        int media = nota;
+        printf("Média do aluno eh %d\n", media);
+        if (media >= 7)
+        {
+            printf("Aluno aprovado\n");
+        }
+        else
+        {
+            printf("Aluno reprovado\n");
+        }
+
+        indice = indice + 1;
+    }
+}
