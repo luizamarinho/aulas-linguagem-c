255be9b1e97a07cbbafd32e512f2a9d781d5bf79
Luiza Marinho <luiza.calheirosm@gmail.com>
2024-09-18 22:13:54 -0300

    Update exercicio-2.c

--- prova-2/exercicio-2.c
+++ prova-2/exercicio-2.c
@@ -2,3 +2,36 @@
 // (excluindo ele mesmo). Escreva um programa que solicite ao usuário um número
 // inteiro positivo e verifique se esse número é perfeito. O programa deve utilizar um
 // laço para somar os divisores e, no final, informar se o número é perfeito ou não
+
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+    int indice = 1;
+    int soma = 0;
+
+    printf("Insira um numero ");
+    scanf("%d", &numero);
+
+    while (indice < numero)
+    {
+
+        if (numero % indice == 0)
+        {
+            soma = soma + indice;
+        }
+        indice = indice + 1;
+    }
+
+    if (soma == numero)
+    {
+        printf("%d eh um numero perfeito\n", numero);
+    }
+    else
+    {
+        printf("%d nao eh um numero perfeito\n", numero);
+    }
+
+    return 0;
+}
