298a4b5ed69fae375d54b332d6926dcb92793397
Luiza Marinho <luiza.calheirosm@gmail.com>
2024-09-18 22:13:57 -0300

    Update exercicio-1.c

--- prova-2/exercicio-1.c
+++ prova-2/exercicio-1.c
@@ -11,10 +11,8 @@ int main()
     float valor_inicial;
     float juros;
     int anos;
-    int indice;
     float rendimento;
-
-    indice = 1;
+    int indice = 1;
 
     printf("Insira o valor inicial do investimento ");
     scanf("%f", &valor_inicial);
@@ -23,11 +21,16 @@ int main()
     printf("Insira o número de anos que o dinheiro será investido ");
     scanf("%d", &anos);
 
+    float montante = valor_inicial;
+
     while (indice <= anos)
     {
-        rendimento = valor_inicial * (juros / 100);
-        valor_inicial = valor_inicial + rendimento;
-        printf("O valor total do investimento ao final do ano %d é %.2f\n", indice, valor_inicial);
+
+        rendimento = montante * (juros / 100);
+        montante = montante + rendimento;
+        printf("O valor total do investimento ao final do ano %d é %.2f\n", indice, montante);
         indice = indice + 1;
     }
+
+    return 0;
 }
