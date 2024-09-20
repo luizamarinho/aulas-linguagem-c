0cd6faca888711a238096d1a48421aa41a5cad65
Luiza Marinho <luiza.calheirosm@gmail.com>
2024-09-20 17:27:23 -0300

    Update exercicio-8.c

--- prova-2/exercicio-8.c
+++ prova-2/exercicio-8.c
@@ -4,3 +4,27 @@
 // para acertar. No final, informe se o usuário acertou ou não. (0.3 ponto)
 // Entrada: palpites do usuário.
 // Saída: dicas (maior/menor) e resultado final (acertou ou esgotou tentativas).
+
+// rand * (final - inicial) + inicial
+// rand * (final - 0) + 0
+// rand * final
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int numero = time(0);
+    srand(numero);
+
+    // chamando uma vez pra aquecer o rand e maximizar a entropia
+    rand();
+
+    float rand_max = RAND_MAX; // o RAND_MAX é int. A operação será feita de maneira inteira, então vai desprezar o número com virgula. Preciso que ele seja float para gerar o número dentro do intervalo 0-1
+
+    float aleatorio_f = (rand() / rand_max) * 100; // o resultado da divisão está entre zero e 1. Posteriormente ele multiplica por 100 pra aumentar o intervalo para 0 - 100
+    int aleatorio = aleatorio_f;                   // converte de float para int. despreza a casa decimal sem arredondar.
+
+    printf("%d\n", aleatorio);
+}
