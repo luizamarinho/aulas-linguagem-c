1b3ffb56df56d22b5d45959ff31104b49e529797
Luiza Marinho <luiza.calheirosm@gmail.com>
2024-09-19 22:46:41 -0300

    Create exercicio-8.c

---
+++ prova-2/exercicio-8.c
@@ -0,0 +1,6 @@
+// Escreva um programa que sorteie um número entre 1 e 100 e permita ao usuário
+// tentar adivinhar o número. O programa deve dar dicas se o número sorteado é
+// maior ou menor que o palpite do usuário. O usuário tem no máximo 7 tentativas
+// para acertar. No final, informe se o usuário acertou ou não. (0.3 ponto)
+// Entrada: palpites do usuário.
+// Saída: dicas (maior/menor) e resultado final (acertou ou esgotou tentativas).
