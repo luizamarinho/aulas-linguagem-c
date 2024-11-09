4bcf4571b48ababd23d353ef5a3e8d7df1cb41f1
Luiza Marinho <luiza.calheirosm@gmail.com>
2024-11-09 13:28:12 -0300

    Create aep.c

---
+++ aep.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int alternativa;
+
+    printf("Insira a opcao que deseja:\n1-Incluir novo usuario.\n2-Editar usuario existente.\n3-Excluir usuario existente.\n4-Listar usuários existentes.\n");
+    scanf("%d", &alternativa);
+}
